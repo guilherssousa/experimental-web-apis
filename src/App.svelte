6c63e10948476178899dfb085706fe06fbc7a6be
Guilherme Sousa <guilherssousa@gmail.com>
2023-08-12 19:00:43 -0300

    refactor home into a screen directory

--- src/App.svelte
+++ src/App.svelte
@@ -1,42 +1,5 @@
 <script>
-  import { checkAvailable } from "./utils";
-
-  const availableApis = checkAvailable([
-    "BarcodeDetector",
-    "Sanitizer",
-    "ViewTransition",
-  ]);
+  import Home from "./screens/Home.svelte";
 </script>
 
-<main class="bg-white min-h-screen">
-  <div class="mx-auto max-w-screen-lg">
-    <div class="mt-8">
-      <h1 class="text-4xl font-bold text-center text-cyan-900">
-        🧪 Laboratório
-      </h1>
-      <p class="text-center text-cyan-900">
-        Teste exemplos de Web APIs experimentais e novas funcionalidades do
-        JavaScript.
-      </p>
-    </div>
-    <div>
-      <h2>Experimentos disponíveis</h2>
-
-      <ul>
-        {#each Object.entries(availableApis) as [api, available], index (api)}
-          <li>
-            {#if available}
-              <a href="https://pudim.com/">
-                {api} ✅
-              </a>
-            {:else}
-              <span>
-                {api} ⛔
-              </span>
-            {/if}
-          </li>
-        {/each}
-      </ul>
-    </div>
-  </div>
-</main>
+<Home />
---
+++ src/screens/Home.svelte
@@ -0,0 +1,41 @@
+<script>
+  import { checkAvailable } from "../utils";
+
+  const availableApis = checkAvailable([
+    "BarcodeDetector",
+    "Sanitizer",
+    "ViewTransition",
+  ]);
+</script>
+
+<main class="bg-white min-h-screen text-neutral-900">
+  <div class="mx-auto max-w-screen-lg px-2">
+    <div class="mt-8">
+      <h1 class="text-4xl font-bold text-center">🧪 Laboratório</h1>
+      <p class="text-center mt-2 text-neutral-700">
+        Teste exemplos de Web APIs experimentais e novas funcionalidades do
+        JavaScript.
+      </p>
+    </div>
+
+    <div class="mt-8">
+      <h2 class="font-bold text-2xl">Experimentos disponíveis</h2>
+
+      <ul>
+        {#each Object.entries(availableApis) as [api, available], index (api)}
+          <li>
+            {#if available}
+              <a href="https://pudim.com/">
+                {api} ✅
+              </a>
+            {:else}
+              <span>
+                {api} ⛔
+              </span>
+            {/if}
+          </li>
+        {/each}
+      </ul>
+    </div>
+  </div>
+</main>
