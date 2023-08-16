db2801d872058293800e0f2fe0471f0279782c25
Guilherme Sousa <guilherssousa@gmail.com>
2023-08-16 12:12:31 -0300

    start working on sanitizer page, move experiment list to astro component

---
+++ src/components/ExperimentTitle.svelte
@@ -0,0 +1,15 @@
+<script>
+    export let experimentId;
+    const available = experimentId in window;
+</script>
+
+<!--
+    Esse arquivo existe apenas para inferir a disponibilidade
+    do experimento em client-side. Infelizmente no Astro,
+    não é possível fazer isso em tempo de build, e, mesmo
+    usando uma tag <script/> para resolver o problema em client-side,
+    o Astro apenas roda o código uma única vez, então
+    essa gambiarra acaba sendo uma saída para o problema.
+-->
+
+{available ? "✅" : "❌"} <slot />
